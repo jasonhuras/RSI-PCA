7b340cf60d98abebb0167f7614cebf55956d750f
Jason Huras <hurasj@uoguelph.ca>
2024-07-12 11:27:00 -0400

    reformatting

--- model_in_sample.py
+++ model_in_sample.py
@@ -6,7 +6,9 @@ from scipy import linalg as la
 import matplotlib.pyplot as plt
 
 
-def pca_linear_model(x: pd.DataFrame, y: pd.Series, n_components: int, thresh: float= 0.01):
+def pca_linear_model(
+    x: pd.DataFrame, y: pd.Series, n_components: int, thresh: float = 0.01
+):
     # Center data at 0
     means = x.mean()
     x -= means
@@ -14,52 +16,48 @@ def pca_linear_model(x: pd.DataFrame, y: pd.Series, n_components: int, thresh: f
 
     # Find covariance and compute eigen vectors
     cov = np.cov(x, rowvar=False)
-    evals , evecs = la.eigh(cov)
+    evals, evecs = la.eigh(cov)
     # Sort eigenvectors by size of eigenvalue
     idx = np.argsort(evals)[::-1]
-    evecs = evecs[:,idx]
+    evecs = evecs[:, idx]
     evals = evals[idx]
 
     # Create data set for model
     model_data = pd.DataFrame()
     for j in range(n_components):
-         model_data['PC' + str(j)] = pd.Series( np.dot(x, evecs[j]) , index=x.index)
-    
-    cols = list(model_data.columns)
-    model_data['target'] = y
-    model_coefs = la.lstsq(model_data[cols], y)[0]
-    model_data['pred'] = np.dot( model_data[cols], model_coefs)
+        model_data["PC" + str(j)] = pd.Series(np.dot(x, evecs[j]), index=x.index)
 
-    l_thresh = model_data['pred'].quantile(0.99)
-    s_thresh = model_data['pred'].quantile(0.01)
+    cols = list(model_data.columns)
+    model_data["target"] = y
+    model_coefs = la.lstsq(model_data[cols], y)[0]
+    model_data["pred"] = np.dot(model_data[cols], model_coefs)
+
+    l_thresh = model_data["pred"].quantile(0.99)
+    s_thresh = model_data["pred"].quantile(0.01)
 
     return model_coefs, means, l_thresh, s_thresh, model_data
 
 
-
-data = pd.read_csv('BTCUSDT3600.csv')
-data['date'] = data['date'].astype('datetime64[s]')
-data = data.set_index('date')
-data = data[data.index < '2020-01-01']
+data = pd.read_csv("BTCUSDT3600.csv")
+data["date"] = data["date"].astype("datetime64[s]")
+data = data.set_index("date")
+data = data[data.index < "2020-01-01"]
 
 rsi_periods = list(range(2, 25))
 rsis = pd.DataFrame()
 for p in rsi_periods:
-    rsis[p] = ta.rsi(data['close'], p)
+    rsis[p] = ta.rsi(data["close"], p)
 
-target = np.log(data['close']).diff(6).shift(-6)
+target = np.log(data["close"]).diff(6).shift(-6)
 
 # Drop nans
-rsis['tar'] = target
+rsis["tar"] = target
 rsis = rsis.dropna()
-target = rsis['tar']
-rsis = rsis.drop('tar',axis=1)
-coefs, means, l_thresh, s_thresh, model_data =  pca_linear_model(rsis, target, 3)
+target = rsis["tar"]
+rsis = rsis.drop("tar", axis=1)
+coefs, means, l_thresh, s_thresh, model_data = pca_linear_model(rsis, target, 3)
 
-plt.style.use('dark_background')
-model_data.plot.scatter('pred', 'target')
-plt.axhline(0.0, color='white')
+plt.style.use("dark_background")
+model_data.plot.scatter("pred", "target")
+plt.axhline(0.0, color="white")
 plt.show()
-
-
-
--- rsi_behavior.py
+++ rsi_behavior.py
@@ -4,20 +4,22 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('BTCUSDT3600.csv')
-data['date'] = data['date'].astype('datetime64[s]')
-data = data.set_index('date')
+data = pd.read_csv("BTCUSDT3600.csv")
+data["date"] = data["date"].astype("datetime64[s]")
+data = data.set_index("date")
 
 rsi_periods = list(range(2, 25))
 
 rsis = pd.DataFrame()
 for p in rsi_periods:
-    rsis[p] = ta.rsi(data['close'], p)
+    rsis[p] = ta.rsi(data["close"], p)
 
-plt.style.use('dark_background')
+plt.style.use("dark_background")
 
 rsis.hist(bins=100)
-plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.5)
+plt.subplots_adjust(
+    left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.5
+)
 plt.show()
 
 rsis.plot()
@@ -27,4 +29,3 @@ plt.show()
 sns.heatmap(rsis.corr(), annot=True)
 plt.xlabel("RSI Period")
 plt.ylabel("RSI Period")
-
